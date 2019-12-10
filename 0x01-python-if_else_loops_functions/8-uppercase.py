1638ae99350e4fb49364f3558b05c85674c0b903
evertcolombia <evertescalante@gmail.com>
2019-12-10 21:53:55 +0000

    8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str2 = ""
+    for char in str:
+        ascii_char = ord(char)
+        if(ascii_char > 96) and (ascii_char < 123):
+            ascii_char -= 32
+            str2 += chr(ascii_char)
+        elif ascii_char > 64 and ascii_char < 91:
+            str2 += chr(ascii_char)
+        elif ascii_char == 32:
+            str2 += chr(ascii_char)
+        elif ascii_char > 47 and ascii_char < 58:
+            str2 += chr(ascii_char)
+    print("{}\n".format(str2), end='')
