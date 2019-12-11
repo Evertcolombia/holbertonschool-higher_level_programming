2cb0d360c4d6d5925f4222949df3fb5bb0dda63f
evertcolombia <evertescalante@gmail.com>
2019-12-11 03:12:08 +0000

    14-smile the mirror exercise

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+listint_t *insert_node(listint_t **head, __attribute((unused))int number)
+{
+	/*listint_t *tmp;*/
+	listint_t *current;
+
+	if (!head)
+		return (0);
+	current = *head;
+
+	while(current->next)
+	{
+		printf("hola");
+		current = current->next;
+	}
+	return (current);
+}
