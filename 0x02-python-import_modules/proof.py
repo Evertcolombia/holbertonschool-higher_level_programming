526c483777a8fa728fd22cfcda1fd968eff1dcf6
evertcolombia <evertescalante@gmail.com>
2019-12-13 03:23:20 +0000

    #pythoniscool

--- 0x02-python-import_modules/proof.py
+++ 0x02-python-import_modules/proof.py
@@ -3,9 +3,9 @@ if __name__ == '__main__':
     import sys
     from calculator_1 import add, sub, mul, div
 
-    op_fun = [
+    operators = [
         ["+", add],
-        ["-", sub],
+        ["-", sub], 
         ["*", mul],
         ["/", div]
     ]
@@ -19,11 +19,12 @@ if __name__ == '__main__':
         op = sys.argv[2]
         b = int(sys.argv[3])
 
-        while i < len(op_fun):
-            if op == op_fun[i][0]:
-                execute = op_fun[i][1](a, b)
+        while i < len(operators):
+            if op == operators[i][0]:  
+                execute = operators[i][1](a, b)
                 print('{:d} {} {:d} = {:d}'.format(a, op, b, execute))
-                sys.exit(0)
+                sys.exit(0) 
             i += 1
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+
