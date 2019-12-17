c745fe0238d00bf5cea36b6a9635b1e677ad0ad5
evertcolombia <evertescalante@gmail.com>
2019-12-17 18:36:17 +0000

    1-hotfix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,9 +4,10 @@ def element_at(my_list, idx):
 
     if (idx < 0):
         return None
-    if (idx > len_list):
+
+    if (idx > len(my_list) - 1):
         return None
 
-    for i in range(len_list):
+    for i in range(len(my_list) - 1):
         if i == idx:
-            return str(my_list[i])
+            return my_list[i]
