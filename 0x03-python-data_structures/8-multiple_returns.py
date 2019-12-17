c0c20b40e5d2a70aed4540537b4ad629c9d1a6b6
evertcolombia <evertescalante@gmail.com>
2019-12-17 23:23:18 +0000

    8-hotfix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,10 +3,10 @@ def multiple_returns(sentence):
     len_str = 0
     char_first = ''
 
-    if (sentence):
+    if not sentence:
+        tuple_a = (0, None)
+    else:
         len_str = len(sentence)
         char_first = sentence[0]
         tuple_a = (len_str, char_first)
         return tuple_a
-    else:
-        tuple_a = (0, None)
