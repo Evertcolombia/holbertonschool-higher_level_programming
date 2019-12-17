d550fd7b398aaeba3e82ad4e6bf3d1276445a245
evertcolombia <evertescalante@gmail.com>
2019-12-17 21:24:43 +0000

    6-hotfix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     if (matrix):
         for i in matrix:
             for j in i:
-                print("{:d}".format(j), end=' ')
-            print()
+                if (j == i[-1]):
+                    print("{:d}".format(j))
+                else:
+                    print("{:d}".format(j), end=' ')
+    
