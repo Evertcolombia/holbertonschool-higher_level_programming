e147238f7b47a93e79ed95ade171156fc094dbbc
evertcolombia <evertescalante@gmail.com>
2019-12-18 03:43:06 +0000

    hotfix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    len_str = 0
-    char_first = ''
-
-    if sentence:
-        len_str = len(sentence)
-        char_first = sentence[0]
-        tuple_a = (len_str, char_first)
-        return tuple_a
+    if len(sentence) == 0:
+        return (0, None)
+    return (len(sentence), sentence[0])
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     tmp = 0
-    if not my_list:
+    if len(my_list):
         return None
-
-    for i in range(len(my_list) - 1):
-        if my_list[i] > my_list[i + 1]:
-            if my_list[i] > tmp:
-                tmp = my_list[i]
+    tmp = my_list[0]
+    for i in my_list:
+        if tmp < i:
+            tmp = i
     return tmp
