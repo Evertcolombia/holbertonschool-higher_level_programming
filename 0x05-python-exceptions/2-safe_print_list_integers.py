45f33c8764a9432effb287987b7458265162e78a
evertcolombia <evertescalante@gmail.com>
2020-01-09 03:23:09 +0000

    hotfix

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyrhon3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for num in range(x):
+        try:
+            print("{:d}".format(my_list[num]), end="")
+            counter += 1
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+        except IndexError:
+            raise
+    print("")
+    return counter
