8d39d3c61e0d4d87f615a1a0d30b6f5e2198b6e4
evertcolombia <evertescalante@gmail.com>
2020-01-12 15:50:23 +0000

    hotfix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,34 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    result = 0
-    num_len = len(roman_string)
-    romans = [
-        ["I", 1],
-        ["V", 5],
-        ["X", 10],
-        ["L", 50],
-        ["C", 100],
-        ["D", 500],
-    ]
-
-    if roman_string is None:
-        return 0
-
-    if num_len == 1:
-        new = [x for x in romans if roman_string[0] in x][0]
-        result += new[1]
-        return result
-
-    for i in range(num_len - 1):
-        for j in range(len(romans)):
-            if roman_string[i] == romans[j][0]:
-                if roman_string[i] == "I":
-                    if roman_string[i + 1] != "I":
-                        new = [x for x in romans if roman_string[i+1] in x][0]
-                        result += new[1]
-                        result -= 1
-                    else:
-                        result += 2
-                else:
-                    result += romans[j][1]
-    return result
+    if roman_string == "" or type(roman_string) is not str:
+        return(0)
+    else:
+        r_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        val = 0
+        for i in range(len(roman_string)):
+            if i > 0 and r_n[roman_string[i]] > r_n[roman_string[i - 1]]:
+                val += r_n[roman_string[i]] - 2 * r_n[roman_string[i - 1]]
+            else:
+                val += r_n[roman_string[i]]
+        return (val)
