bf8cc1cb74462b58a773dff4d0f8759bf6dc0c68
evertcolombia <evertescalante@gmail.com>
2020-01-12 02:09:40 +0000

    hotfix

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,23 @@
+#include "/usr/include/python3.4/Python.h"
+#include "/usr/include/python3.4/object.h"
+#include "/usr/include/python3.4/listobject.h"
+#include <stdio.h>
+
+void print_phyton_list_info(PyObject *p)
+{
+	int len = 0, idx;
+	PyObject *item;
+	PyListObject *new = (PyListObject *) p;
+
+	len = Py_SIZE(p);
+	printf("[*] Size of the Python List = %d\n", len);
+	printf("[*] Allocated = %d\n", (int) new->allocated);
+
+	for (idx = 0; idx < len; idx++)
+	{
+		item = PyList_GET_ITEM(p, idx);
+		printf("Element %d: %s\n", idx, item->ob_type->tp_name);
+	}
+
+	return;
+}
