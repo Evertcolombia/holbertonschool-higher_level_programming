a0a20a8907ec21af521db62b8ddc94ef56131ed6
evertcolombia <evertescalante@gmail.com>
2020-01-12 03:43:25 +0000

    hotif

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,23 +1,18 @@
-#include "/usr/include/python3.4/Python.h"
-#include "/usr/include/python3.4/listobject.h"
-#include "/usr/include/python3.4/object.h"
+#include <Python.h>
 #include <stdio.h>
 
 void print_phyton_list_info(PyObject *p)
 {
 	int len = 0, idx;
 	PyObject *item;
-	PyListObject *clone = (PyListObject *) p;
 
 	len = Py_SIZE(p);
 	printf("[*] Size of the Python List = %d\n", len);
-	printf("[*] Allocated = %d\n", (int) clone->allocated);
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 
 	for (idx = 0; idx < len; idx++)
 	{
-		item = PyList_GET_ITEM(p, idx);
-		printf("Element %d: %s\n", idx, item->ob_type->tp_name);
+		item = PyList_GetItem(p, idx);
+		printf("Element %d: %s\n", idx, Py_TYPE(item)->tp_name);
 	}
-
-	return;
 }
