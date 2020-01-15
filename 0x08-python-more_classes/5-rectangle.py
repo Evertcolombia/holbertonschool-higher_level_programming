9644043065a6d8eae7e31940dffc13bd3954417a
evertcolombia <evertescalante@gmail.com>
2020-01-15 04:46:09 +0000

    hotfix of comments

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,25 +1,46 @@
 #!/usr/bin/python3
 class Rectangle:
     """
-    Class definition: Rectangle
+    The Rectangle clas create a new object
+
+    Args:
+        width (int): integer value of the width
+        height (int): integer value of the height
 
     Attributes:
-    __width (int): Rectangle width  private
+        width (int): integer value of the width
+        height (int): integer value of the height
+        number_of_instances (int): integer number of the instances
+        print_symbol (str): print with the symbol
 
     """
+
     def __init__(self, width=0, height=0):
-        """
-        Args:
-            width: Rectangle  width
-            height: Rectangle height
-        """
         self.__height = height
         self.__width = width
 
     def area(self):
+        """
+        Function area
+
+        Formule: width * height = area
+
+        Returns:
+            area
+
+        """
         return self.__width * self.__height
 
     def perimeter(self):
+        """
+        Function perimeter
+
+        Formula: (width * 2) + (height * 2)
+
+        Returns:
+            perimeter
+
+        """
         if self.__width == 0 or self.__height == 0:
             return 0
 
@@ -27,10 +48,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """ Get width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -39,10 +62,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """ Get height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set height"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -50,6 +75,7 @@ class Rectangle:
         self.__height = value
 
     def __str__(self):
+        """Returns an string simulating the instance"""
         string = ""
         if self.__width == 0 or self.__height == 0:
             return string
@@ -59,7 +85,9 @@ class Rectangle:
             return string[:-1]
 
     def __repr__(self):
+        """Returns an stringre presentation the instance"""
         return ("Rectangle({}, {})".format(self.__width, self.__height))
 
     def __del__(self):
+        """del object print"""
         print("Bye rectangle...")
