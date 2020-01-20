70f0e82fb257cfbdf90acb5a11b288a4447f7831
evertcolombia <evertescalante@gmail.com>
2020-01-20 23:22:52 +0000

    9-definingstr method

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,17 +6,13 @@ class BaseGeometry:
     Class definition: BaseGeometry
     """
     def area(self):
-        """
-        Raises:
-            Exception
-        """
-        raise Exception("area() is not implemented")
+         
 
     def integer_validator(self, name, value):
         self.name = name
         self.value = value
 
-        if type(value) is not int:
+        if type(self.value) != int:
             raise TypeError(self.name + " must be an integer")
         if value < 1:
             raise ValueError(self.name + " must be grather than 0")
@@ -28,4 +24,7 @@ class Rectangle(BaseGeometry):
     """
     def __init__(self, width, height):
         self.integer_validator("width", width)
-        self.integer_validator("height", height)       
+        self.integer_validator("height", height)
+
+    def __str__(self):
+        return '[Rectangle] {self.__width} / {self.height}'.format(self(self=self))
