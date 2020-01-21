f93aaf9109839e76292f6f5a340438bcd14a32fd
evertcolombia <evertescalante@gmail.com>
2020-01-21 23:27:16 +0000

    hotfix

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+save_to__json_file = __import__('8-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+add_list = []
+length = len(sys.argv)
+file_name = "add_item.json"
+
+if length >= 2:
+
+    try:
+        file_python = load_from_json_file(file_name)
+        for i in range(length):
+            add_list[i] = sys.argv[i]
+        file_json = save_to_json_file(add_list, file_python)
+        return load_from_json_file(file_json)
+    except:
+        pass
+else:
+    return add_list
+
+
