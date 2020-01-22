15f1dcf02714a239ee5cbb77222ac9f7fc4f6f60
evertcolombia <evertescalante@gmail.com>
2020-01-22 01:09:53 +0000

    hotfix

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            d = self.__dict__
+            return {z: d[z] for atr in attrs for z in d if z == atr}
+
+    def reload_from_json(self, json):
+        for key in json:
+           
