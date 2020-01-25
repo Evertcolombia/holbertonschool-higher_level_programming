b872fbc77a8604b40b0488bb25d1095a1588452f
evertcolombia <evertescalante@gmail.com>
2020-01-25 20:28:54 +0000

    to_Dictionary method in Rectangle class

---
+++ 0x0C-python-almost_a_circle/models/2-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format( \
+        self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.validation("width", value)
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        if len(args) > 0 and args is not None:
+            self.id = args[0]
+            for i, arg in enumerate(args):
+                if (i == 1):
+                    self.size = arg
+                if (i == 2):
+                    self.x = arg
+                if (i == 3):
+                    self.y = arg
+
+        elif len(kwargs) > 0 and kwargs is not None:
+            for k, v in kwargs.items():
+                if (k == "id"):
+                    self.id = v
+                if (k == "size"):
+                    self.size = v
+                if (k == "x"):
+                    self.x = v
+                if (k == "y"):
+                    self.y = v
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from models.base import Base
+import json
 
 
 class Rectangle(Base):
@@ -96,3 +97,11 @@ class Rectangle(Base):
                     self.x = v
                 if (k == "y"):
                     self.y = v
+
+    def to_dictionary(self):
+        l = ["id", "width", "height", "x", "y"]
+        d = {}
+        for attr in l:
+            a = {attr: getattr(self, attr)}
+            d.update(a)
+        return d
