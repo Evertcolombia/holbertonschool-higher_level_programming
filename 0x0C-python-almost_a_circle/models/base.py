68cb02f0472b4b1e11946d3197980302edfa1f03
evertcolombia <evertescalante@gmail.com>
2020-01-27 21:31:44 +0000

    hotfix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -100,9 +100,9 @@ class Base:
         """
         if cls.__name__ == 'Rectangle':
             dummy = cls(1, 1, 1, 1,)
+            dummy.update(**dictionary)
         elif cls.__name__ == 'Square':
             dummy = cls(9)
-        dummy.update(**dictionary)
         return dummy
 
     @classmethod
