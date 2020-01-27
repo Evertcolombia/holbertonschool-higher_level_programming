e405594614aaa3f460988cf0bf45f683de2b1b61
evertcolombia <evertescalante@gmail.com>
2020-01-27 23:38:14 +0000

    hotfix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -124,3 +124,52 @@ class Base:
             return empty_list
         except IOError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_obj):
+        """classmethod save to file
+            save a a file with a list of obects converted in dictionaries
+
+            Args:
+                param1 (cls): class reference
+                parama2 (list_obj): list of objects
+
+        """
+        name = cls.__name__
+        filename = str(name + ".csv")
+
+        with open(filename, mode='w', encoding='utf-8') as f:
+            flag = 0
+
+            if list_obj is None:
+                f.write(json.dumps([]))
+            else:
+                f.write("[")
+                for el in list_obj:
+                    if flag > 0:
+                        f.write(", ")
+                    a = cls.to_json_string(el.to_dictionary())
+                    flag += 1
+                    f.write(a)
+                f.write("]")
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+            calculate area of rectangle
+        """
+        name = cls.__name__
+        filename = str(name + ".csv")
+
+        try:
+            with open(filename, mode='r', encoding='utf-8') as f:
+                lines = f.read()
+                list_dictionaries = cls.from_json_string(lines)
+            empty_list = []
+
+            for dic in list_dictionaries:
+                obj = cls.create(**dic)
+                empty_list.append(obj)
+            return empty_list
+        except IOError:
+            return []
