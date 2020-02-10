e226a1761e60516a0efc7bce70ac37ab671b7459
evertcolombia <evertescalante@gmail.com>
2020-02-10 05:25:10 +0000

    hotfix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new = [0, 0]
     if (tuple_a):
-        if len(tuple_a) == 2:
+        if len(tuple_a) >= 2:
             for i in range(2):
                 if tuple_a[i]:
                     new[i] += tuple_a[i]
@@ -10,7 +10,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
             new[0] += tuple_b[0]
 
     if (tuple_b):
-        if len(tuple_b) == 2:
+        if len(tuple_b) >= 2:
             for i in range(len(tuple_b)):
                 if tuple_b[i]:
                     new[i] += tuple_b[i]
