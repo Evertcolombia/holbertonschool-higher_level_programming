02d99c56d269ffc0014834c4430f821f26668a1f
evertcolombia <evertescalante@gmail.com>
2020-02-24 02:41:47 +0000

    hotfix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,8 +2,17 @@
 
 import sys
 
+def ten_times(file_size, allowed_codes):
+
+    print("File size: {:d}".format(file_size))
+
+    for key in sorted(allowed_codes.keys()):
+        val = allowed_codes.get(key)
+        if val != 0:
+            print("{}: {:d}".format(key, val))
+
 allowed_codes = {
-    '200': 0, '301': 0, '400': 0, '401': 0, 
+    '200': 0, '301': 0, '400': 0, '401': 0,
     '403': 0, '404': 0, '405': 0, '500': 0
 }
 
@@ -22,10 +31,7 @@ for line in sys.stdin:
         allowed_codes[status_code] += 1
 
     if count == 10:
+        ten_times(file_size, allowed_codes)
         count = 0
-        print("File size: {}".format(file_size))
 
-        for key in sorted(allowed_codes.keys()):
-            val = allowed_codes.get(key)
-            if val != 0:
-                print("{}: {}".format(key, val))
+
