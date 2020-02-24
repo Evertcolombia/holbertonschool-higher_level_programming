54aca4f572b3de4a8bf2028325453a49344eea77
evertcolombia <evertescalante@gmail.com>
2020-02-24 05:07:06 +0000

    hotfix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 import sys
+from signal import signal, SIGINT
+
+
+count, file_size = 0, 0
+allowed_codes = {
+    '200': 0, '301': 0, '400': 0, '401': 0,
+    '403': 0, '404': 0, '405': 0, '500': 0
+}
 
 def ten_times(file_size, allowed_codes):
 
@@ -11,33 +19,28 @@ def ten_times(file_size, allowed_codes):
         if val != 0:
             print("{}: {:d}".format(key, val))
 
-allowed_codes = {
-    '200': 0, '301': 0, '400': 0, '401': 0,
-    '403': 0, '404': 0, '405': 0, '500': 0
-}
-
-count = 0
-file_size = 0
-
-try:
-    for line in sys.stdin:
-
-        if count == 10:
-            ten_times(file_size, allowed_codes)
-            count = 0
-
-        split_line = line.split()
-        if split_line[7] and split_line[7] in allowed_codes.keys():
-            allowed_codes[split_line[7]] += 1
-
-        try:
-            file_size += int(split_line[8])
-        except:
-            pass
-
-        count += 1
-
+def handler(signal_received, frame):
     ten_times(file_size, allowed_codes)
-except KeyboardInterrupt:
-    ten_times(file_size, allowed_codes)
-    raise
+    sys.exit(0)
+
+for line in sys.stdin:
+
+    signal(SIGINT, handler)
+
+    if count == 10:
+        ten_times(file_size, allowed_codes)
+        count = 0
+
+    split_line = line.split()
+    if split_line[7] and split_line[7] in allowed_codes.keys():
+        allowed_codes[split_line[7]] += 1
+
+    try:
+        size = int(split_line[8])
+        file_size += size
+    except:
+        pass
+
+    count += 1
+
+ten_times(file_size, allowed_codes)
