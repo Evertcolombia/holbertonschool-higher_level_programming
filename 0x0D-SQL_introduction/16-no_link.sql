3cc0919f226066bed9c80224e86dce3c83384274
evertcolombia <evertescalante@gmail.com>
2020-03-02 23:58:48 +0000

    where name is not null condtion

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- list all record with based in condition
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
