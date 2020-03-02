50c382cc0b940e606628c7a839f319eabb0b0d25
evertcolombia <evertescalante@gmail.com>
2020-03-02 21:42:40 +0000

    full table script sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- Full creation of a table
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table(id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table(id, name, score) value (3, "Bob", 14);
+INSERT INTO second_table(id, name, score) value (4, "GEORGE", 8);
+
