152824ec3a4b1d4ddef7e0285601fc45f50dccdf
evertcolombia <evertescalante@gmail.com>
2020-03-02 21:46:19 +0000

    hotfix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Full creation of a table
-CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table(id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table(id, name, score) VALUES (2, "Alex", 3);
 INSERT INTO second_table(id, name, score) value (3, "Bob", 14);
