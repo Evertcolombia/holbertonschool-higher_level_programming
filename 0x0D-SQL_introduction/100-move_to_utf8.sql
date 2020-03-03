8354137fcd395eadc98d4a3943d257e667bb8c06
evertcolombia <evertescalante@gmail.com>
2020-03-03 00:19:49 +0000

    convet to unicode

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- move to utf8 database and table
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
