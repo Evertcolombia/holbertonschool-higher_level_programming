3d31433f2b33b4ef8df2409ab752c161325467d1
evertcolombia <evertescalante@gmail.com>
2020-03-29 16:16:39 +0000

    hotfix

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    s_name = sys.argv[4]
+    st = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(st.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    Session = Session()
+
+    query = Session.query(State).filter(State.name==s_name)
+    
+    for el in query:
+        print(el.id)
