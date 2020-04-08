f4f1ef39697c807a59e63ee5b29827a89053bcee
evertcolombia <evertescalante@gmail.com>
2020-04-08 00:09:16 +0000

    sending a delete request to a server with  curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # shot Content-Length of a response header from a request
-curl -Is $1 | grep "Content-Length:" | awk '{print $2}'
+curl -Is "$1" | grep "Content-Length:" | awk '{print $2}'
