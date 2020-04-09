eefebd2c57e76670b5de300c70cce6a2934521d7
evertcolombia <evertescalante@gmail.com>
2020-04-09 03:46:06 +0000

    hotfix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,13 +9,15 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
 
     s_name = sys.argv[4]
-    st = 'mysql+mysqldb://{}:{}@localhost/{}'
+
+    """st = 'mysql+mysqldb://{}:{}@localhost/{}'
     engine = create_engine(st.format(sys.argv[1], sys.argv[2],
-                                     sys.argv[3]), pool_pre_ping=True)
+                                     sys.argv[3]), pool_pre_ping=True)"""
+   engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     Session = Session()
 
     query = Session.query(State).filter_by(name=s_name).first()
-
     print(query.id) if (query is not None) else print('Not found')
