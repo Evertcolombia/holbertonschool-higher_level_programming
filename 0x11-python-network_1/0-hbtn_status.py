42ba2aef89e29e0ceeeade1c3ef7dca5790378af
evertcolombia <evertescalante@gmail.com>
2020-04-14 17:10:39 +0000

    0- using urllib.request tp get a fetch an url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from urllib import request
+
+url = 'https://intranet.hbtn.io/status'
+with request.urlopen(url) as response:
+    content = response.read()
+
+res_headers = [['type', type(content)],
+               ['content', content],
+               ['utf content', content.decode('utf-8')]]
+
+print('Body Response:')
+for el in res_headers:
+    print('\t- {}: {}'.format(el[0], el[1]))
