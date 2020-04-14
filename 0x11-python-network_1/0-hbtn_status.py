15ffe4e2ab65ffc7392a81e279cb53e7c721712d
evertcolombia <evertescalante@gmail.com>
2020-04-14 22:08:15 +0000

    hotfix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 from urllib import request
 
-url = 'https://intranet.hbtn.io/status'
-with request.urlopen(url) as response:
-    content = response.read()
 
-res_headers = [['type', type(content)],
-               ['content', content],
-               ['utf content', content.decode('utf-8')]]
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    with request.urlopen(url) as response:
+        content = response.read()
 
-print('Body Response:')
-for el in res_headers:
-    print('\t- {}: {}'.format(el[0], el[1]))
+    res_headers = [['type', type(content)],
+                   ['content', content],
+                   ['utf content', content.decode('utf-8')]]
+
+    print('Body Response:')
+    for el in res_headers:
+        print('\t- {}: {}'.format(el[0], el[1]))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,10 @@
 from urllib import request
 import sys
 
-url = sys.argv[1]
-with request.urlopen(url) as response:
-    headers = response.info()
-    if headers['X-Request-Id']:
-        print(headers['X-Request-Id'])
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with request.urlopen(url) as response:
+        headers = response.info()
+        if headers['X-Request-Id']:
+            print(headers['X-Request-Id'])
