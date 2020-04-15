2aa095ce1bb949339052a3517206f75f5c15bff9
evertcolombia <evertescalante@gmail.com>
2020-04-15 17:52:36 +0000

    hotfix3

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
     with request.urlopen(url) as response:
         content = response.read()
 
-        res_headers = [['type', type(content)], ['content', content], ['utf content', content.decode('utf-8')]]
+        res_headers = [['type', str(type(content))],
+            ['content', str(content)],
+            ['utf8 content', content.decode('utf-8')]]
 
         print('Body Response:')
         for el in res_headers:
