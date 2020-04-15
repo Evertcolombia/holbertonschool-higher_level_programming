af08e3f5d931658ae41be03211e55bfb58fbfdf8
evertcolombia <evertescalante@gmail.com>
2020-04-15 18:15:17 +0000

    hotfix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from urllib import request
+import urllib.request
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
-    print('Body response:')
-    print('    - type: {}'.format(type(content)))
-    print('    - content: {}'.format(content))
-    print('    - utf8 content: {}'.format(content.decode('utf-8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode()))
