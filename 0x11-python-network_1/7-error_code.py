64af019bcf33bab1fcd444c8fc6c43b1f334b01e
evertcolombia <evertescalante@gmail.com>
2020-04-15 18:38:14 +0000

    hotfix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,10 +4,9 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = argv[1]
-    res = requests.get(url)
+    res = requests.get(argv[1])
 
     if res.status_code >= 400:
-        print("Error code: {}".format(status_code))
+        print("Error code: {}".format(res.status_code))
     else:
         print(res.text)
