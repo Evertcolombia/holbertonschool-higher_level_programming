0986453b6dc1e157979490ebe965dbf8969d52d0
evertcolombia <evertescalante@gmail.com>
2020-04-14 23:47:24 -0500

    sending data to an api json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+#"http://21701fe1a6fd.19.hbtn-cod.io:5000/search_user"
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {}
+    if argv[1]:
+        payload['q'] = argv[1]
+    else:
+        payload['q'] = ""
+    res = requests.post(url, data=payload)
+    res.raise_for_status()
+
+    try:
+        obj = res.json()
+        try:
+            _id = "[{}] ".format(obj['id'])
+            name = "{}".format(obj['name'])
+            print(_id + name)
+        except:
+            print('No result')
+    except:
+        print('Not a valid JSON')
