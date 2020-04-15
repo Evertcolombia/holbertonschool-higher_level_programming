2fcb2b10f682b8902c9f1962ee0cedb585b9cb0e
evertcolombia <evertescalante@gmail.com>
2020-04-15 18:24:29 +0000

    hotfix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,9 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
-    print("Body response:")
 
-    headers = [["type", type(content)], ["content", content],
-                ["utf8 content", content.decode()]]
+    deco = ["utf8 content", content.decode()]
+    headers = [["type", type(content)], ["content", content], deco]
+    print("Body response:")
     for el in headers:
         print("\t- {}: {}".format(el[0], el[1]))
-    #print("\t- type: {}".format(type(content)))
-    #print("\t- content: {}".format(content))
-    #print("\t- utf8 content: {}".format(content.decode()))
