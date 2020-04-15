13615a52591e16e9adab8b95ebd5828cbb1bb0ad
evertcolombia <evertescalante@gmail.com>
2020-04-15 18:33:28 +0000

    hotfix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,5 +4,5 @@ from sys import argv
 
 if __name__ == "__main__":
     res = requests.get(argv[1])
-    x_request_id = res.headers.get['X-Request-Id']
+    x_request_id = res.headers.get('X-Request-Id')
     print(x_request_id)
