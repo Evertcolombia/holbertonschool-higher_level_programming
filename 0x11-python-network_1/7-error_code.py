3b3c7065ac1ffe91070678fdc0f4b5b9980c48f8
evertcolombia <evertescalante@gmail.com>
2020-04-14 22:25:22 -0500

    hotfix

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    res = requests.get(url)
+
+    if res.status_code >= 400:
+        print("Error code: {}".format(status_code))
+    else:
+        print(res.text)
