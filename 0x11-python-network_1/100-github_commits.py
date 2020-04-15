15c02ce91ddd1c08f80744efe371623626788532
evertcolombia <evertescalante@gmail.com>
2020-04-15 18:57:12 +0000

    hotfix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,15 +4,10 @@ from sys import argv
 import json
 
 if __name__ == "__main__":
-    repo_name = argv[1]
-    username = argv[2]
-    st = 'https://api.github.com/repos/{}/{}/commits'
-    url = st.format(username, repo_name)
+    #st = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+    #url = st.format(username, repo_name)
+    res = requests.get('https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1]))
 
-    res = requests.get(url)
-    res.raise_for_status()
-
-    print(res.text)
     try:
         obj = res.json()
         for el in obj[0:10]:
