373bac3e3b327c2cb716abcd6d675f0c2b900d22
evertcolombia <evertescalante@gmail.com>
2020-04-15 19:12:22 +0000

    hotfix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,11 +3,11 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    res = requests.get('https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1]))
+    res = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
+        argv[2], argv[1]))
 
     obj = res.json()
     for el in obj[:10]:
         sha = el.get('sha')
         author = el.get('commit').get('author').get('name')
         print("{}: {}".format(sha, author))
-
