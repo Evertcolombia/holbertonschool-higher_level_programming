c30bab10f370c9bd7f748770cd7e06ba4ba18327
evertcolombia <evertescalante@gmail.com>
2020-04-15 18:19:22 +0000

    hotfx

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,11 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
     print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode()))
+
+    headers = [["type", type(content)], ["content", content],
+                ["utf8 content", content.decode()]]
+    for el in headers:
+        print("\t- {}: {}".format(el[0], el[1]))
+    #print("\t- type: {}".format(type(content)))
+    #print("\t- content: {}".format(content))
+    #print("\t- utf8 content: {}".format(content.decode()))
