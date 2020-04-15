54c2a8da9662f85cf095674705e06e21e57688cc
evertcolombia <evertescalante@gmail.com>
2020-04-15 18:52:12 +0000

    hotfix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
 
     res = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
 
-    if "id" in res.json():
+    try:
         obj = res.json()
         _id = obj.get('id')
         print(_id)
-    else:
+    except:
         print("None")
