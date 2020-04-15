df2aca61b82d538423982926a33df2ed9d2d6ce6
evertcolombia <evertescalante@gmail.com>
2020-04-15 18:11:48 +0000

    hotfix

--- 0x11-python-network_1/0-proof.py
+++ 0x11-python-network_1/0-proof.py
@@ -4,7 +4,7 @@ from urllib import request
 if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
-        print('Body response:')
-        print('    - type: {}'.format(str(type(content))))
-        print('    - content: {}'.format(str(content)))
-        print('    - utf8 content: {}'.format(content.decode('utf-8')))
+    print('Body response:')
+    print('    - type: {}'.format(type(content)))
+    print('    - content: {}'.format(content))
+    print('    - utf8 content: {}'.format(content.decode('utf-8')))
