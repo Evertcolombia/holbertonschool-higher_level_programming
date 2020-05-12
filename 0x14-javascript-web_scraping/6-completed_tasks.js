d76cd0aa23852573ec977f45885621c4789264fe
evertcolombia <evertescalante@gmail.com>
2020-05-12 18:52:22 -0500

    hotfix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,21 +3,24 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (error, response, body) => {
-  if (error) return console.log('error', error);
-  const data = JSON.parse(body);
-  const results = Object();
-
+function doTask (data, results) {
   for (const x in data) {
     if (data[x].completed) {
       const userId = data[x].userId;
-      const isInResults = results.hasOwnProperty(userId);
-      if (isInResults) {
+      if (results.hasOwnProperty(userId)) {
         results[userId] += 1;
       } else {
         results[userId] = 1;
       }
     }
   }
+  return results;
+}
+
+request(url, (error, response, body) => {
+  if (error) return console.log('error', error);
+  const data = JSON.parse(body);
+  let results = Object();
+  results = doTask(data, results);
   console.log(results);
 });
