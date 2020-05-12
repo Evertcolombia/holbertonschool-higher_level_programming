79546f6bdd6e2fe00783535a4fe7cadd22f33780
evertcolombia <evertescalante@gmail.com>
2020-05-12 17:52:59 -0500

    hotfix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,18 +2,10 @@
 
 const fs = require('fs');
 const request = require('request');
-const url = process.argv[2].concat('/').concat(process.argv[3]);
 
-function write (filename, string) {
-  fs.writeFile(filename, string, 'utf8', (err, data) => {
+request(process.argv[2], (error, response, body) => {
+  if (error) return console.log('error', error);
+  fs.writeFile(process.argv[3], body, 'utf8', (err) => {
     if (err) return console.log(err);
   });
-}
-function doRequest () {
-  request(url, (error, response, body) => {
-    if (error) return console.log('error', error);
-    write(process.argv[3], body);
-  });
-}
-
-doRequest();
+});
