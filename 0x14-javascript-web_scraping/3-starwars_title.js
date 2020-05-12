14d0edaa84f5c642d49be5b557f3a12ad4f4c1be
evertcolombia <evertescalante@gmail.com>
2020-05-12 16:10:50 -0500

    using request to conect an api and download resources

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
 request(url, (error, response, body) => {
   if (error) return console.log('error', error);
-  console.log(`code: ${response.statusCode}`);
+  console.log(JSON.parse(body).title);
 });
