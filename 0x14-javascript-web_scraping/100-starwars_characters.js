ed9f179bf0a902272054eff7d2d5b2e656ed1d14
evertcolombia <evertescalante@gmail.com>
2020-05-12 22:20:22 -0500

    hotfix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,18 +3,15 @@
 const request = require('request');
 const filmsUrl = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-console.log(filmsUrl)
-function doRequest (url) {
-  setTimeout(() => {	
-    request(url, (error, response, body) => {
+request(filmsUrl, (error, response, body) => {
+  if (error) return console.log('error', error);
+  const data = JSON.parse(body).characters;
+
+  for (const x in data) {
+    request(data[x], (error, response, body) => {
       if (error) return console.log('error', error);
-      const data = JSON.parse(body);
-      console.log(data);
-      return data;
+      const character = JSON.parse(body).name;
+      console.log(character);
     });
-  }, 3000);
-}
-
-
-let data = doRequest(filmsUrl);
-console.log(data)
+  }
+});
