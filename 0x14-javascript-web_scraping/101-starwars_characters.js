c3da4b6d3afe025dfcd85e668a9d5fd0a07a1bd7
evertcolombia <evertescalante@gmail.com>
2020-05-13 19:03:38 -0500

    hotfix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,24 +2,33 @@
 
 const request = require('request');
 const filmsUrl = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
-let count = 0;
 
-request(filmsUrl, (error, response, body) => {
-  if (error) return console.log('error', error);
-  const data = JSON.parse(body).characters;
-  const characters = {};
-
-  for (const x in data) {
-    request(data[x], (error, response, body) => {
-      if (error) return console.log('error', error);
-      characters[x] = JSON.parse(body).name;
-      count++;
-
-      if (count === data.length) {
-        for (const v in characters) {
-          console.log(characters[v]);
-        }
-      }
+function doRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response) => {
+      error ? reject(error) : resolve(response);
     });
+  });
+}
+
+async function getFilm (url) {
+  const res = await doRequest(url);
+  return JSON.parse(res.body).characters;
+}
+
+async function getCharacters (data) {
+  for (const x in data) {
+    const res = await doRequest(data[x]);
+    console.log(JSON.parse(res.body).name);
   }
-});
+}
+
+(async () => {
+  let data = '';
+  try {
+    data = await getFilm(filmsUrl);
+    await getCharacters(data);
+  } catch (error) {
+    console.log(error);
+  }
+})();
